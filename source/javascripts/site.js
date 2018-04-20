39f6d4d0395fa5608534fcd2713c04d90e1d75ce
Jacob Patterson <patterson.882@osu.edu>
2018-04-19 23:07:15 -0400

    Initial layout and index page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,8 @@
-// This is where it all goes :)
+document.querySelector('.navbar-burger').addEventListener("click", function(){
+    let menu = document.querySelector('.navbar-menu');
+    if (menu.className == 'navbar-menu') {
+        menu.className = 'navbar-menu is-active';
+    } else {
+        menu.className = 'navbar-menu';
+    }
+})
