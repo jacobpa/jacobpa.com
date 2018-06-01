1ec13e1dd6c70d2f42c453e59b0769c131749f99
Jacob Patterson <jacob@jacobpa.com>
2018-05-31 20:01:28 -0400

    Refactor burger menu javascript

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,5 @@
 document.querySelector('.navbar-burger').addEventListener("click", function(){
     let menu = document.querySelector('.navbar-menu');
-    if (menu.className == 'navbar-menu') {
-        menu.className = 'navbar-menu is-active';
-    } else {
-        menu.className = 'navbar-menu';
-    }
+
+    menu.classList.toggle('is-active');
 });
